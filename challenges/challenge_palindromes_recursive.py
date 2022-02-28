57f9c576904c9b73708f7241537abf3bde2c6f42
Southier <patrick.southier@hotmail.com>
2022-02-28 17:35:35 -0300

    chore: removing empty lines

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0 or word[low_index] != word[high_index]:
         return False
-
     if high_index == 0:
         return True
-
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
