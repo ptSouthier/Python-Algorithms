a178119fab3dc576d7c9202949591ad3d95b2980
Southier <patrick.southier@hotmail.com>
2022-03-01 23:46:53 -0300

    chore: removing the empty lines

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,14 +5,11 @@ def check_entry(entry):
 
 def study_schedule(permanence_period, target_time):
     students_present = 0
-
     check_entry(target_time)
-
     for student in permanence_period:
         try:
             if student[0] <= target_time <= student[1]:
                 students_present += 1
         except TypeError:
             return None
-
     return students_present
