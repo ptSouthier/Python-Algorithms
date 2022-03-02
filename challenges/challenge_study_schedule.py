f5431aeab2c4e7ac3a61ac59cbf3551ddc7dd5f0
Southier <patrick.southier@hotmail.com>
2022-03-01 23:37:24 -0300

    feat: creating the study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_present = 0
+
+    if target_time is None:
+        return None
+
+    for student in permanence_period:
+        try:
+            if student[0] <= target_time <= student[1]:
+                students_present += 1
+        except TypeError:
+            return None
+
+    return students_present
