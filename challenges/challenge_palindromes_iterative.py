d95de5bac458a406fba51d54b6c48151f8c7de97
Southier <patrick.southier@hotmail.com>
2022-03-03 23:39:20 -0300

    feat: trying another return to palindromes_iterative function, but still not working

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,8 +1,4 @@
 def is_palindrome_iterative(word):
     splitted_word = word.split()
 
-    if splitted_word.reverse() == splitted_word:
-        return True
-
-    else:
-        return False
+    return splitted_word.reverse() == splitted_word
