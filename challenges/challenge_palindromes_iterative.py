2fb8b2cfedc3631b17cd599818588e1c8d1311ad
Southier <patrick.southier@hotmail.com>
2022-03-03 23:31:06 -0300

    feat: creating the is_palindrome_iterative function, but with the wrong logic/algorithm yet

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    splitted_word = word.split()
+
+    if splitted_word.reverse() == splitted_word:
+        return True
+
+    else:
+        return False
