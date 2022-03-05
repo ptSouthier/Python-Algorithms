6e0261f6c079fff5e6aee092eafab10e1bb4c7fb
Southier <patrick.southier@hotmail.com>
2022-03-05 17:52:05 -0300

    feat: creating the anagrams function and the bubble_sort as an auxiliar function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,22 @@
+def bubble_sort(list):
+    has_swapped = True
+    num_of_iterations = 0
+
+    while has_swapped:
+        has_swapped = False
+
+        for index in range(len(list) - num_of_iterations - 1):
+            if list[index] > list[index + 1]:
+                list[index], list[index + 1] = list[index + 1], list[index]
+                has_swapped = True
+
+        num_of_iterations += 1
+
+    return list
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    string_a = bubble_sort(first_string.split())
+    string_b = bubble_sort(second_string.split())
+
+    return string_a == string_b
